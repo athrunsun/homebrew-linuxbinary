6205a660007c98411194f6701349c4311db72a57
okampfer <okampfer@gmail.com>
2016-02-20 11:24:04 +0800

    Add datagrip.

---
+++ Formula/datagrip.rb
@@ -0,0 +1,14 @@
+class DataGrip < Formula
+    desc "Your Swiss Army Knife for Databases and SQL."
+    homepage "http://www.jetbrains.com/datagrip/"
+    version "1.0.3"
+    url "https://download.jetbrains.com/datagrip/datagrip-#{version}-custom-jdk-linux.tar.gz"
+    sha256 "996a9d6f3e4bb643ea5470673170e89a6330af1d19728785ba3ea1d7ad008081"
+
+    bottle :unneeded
+
+    def install
+        prefix.install Dir["*"]
+        bin.install_symlink({"#{prefix}/bin/datagrip.sh" => "datagrip"})
+    end
+end
