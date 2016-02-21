34ae2eeb611130b6ccc94d6ea0f9f2abb312f529
okampfer <okampfer@gmail.com>
2016-02-21 17:17:54 +0800

    Fix incorrect sha256 for postgresql-binary.

--- Formula/postgresql-binary.rb
+++ Formula/postgresql-binary.rb
@@ -5,10 +5,10 @@ class PostgresqlBinary < Formula
 
     if MacOS.prefer_64_bit?
         url "http://get.enterprisedb.com/postgresql/postgresql-#{version}-linux-x64-binaries.tar.gz"
-        sha256 "b963cdadf47c1a6f327d1496b813924141c3e81ffc90fa504f64262af438d352"
+        sha256 "1d85f85cd9e8236038b90ce5c078d22bd458fe8f924b46c4d341e3344753c309"
     else
         url "http://get.enterprisedb.com/postgresql/postgresql-#{version}-linux-binaries.tar.gz"
-        sha256 "b963cdadf47c1a6f327d1496b813924141c3e81ffc90fa504f64262af438d352"
+        sha256 "630e6dafedf96e2dc61f6c1e584fdc0debcf9f099ebd5ec05ffbd379d41cec61"
     end
 
     bottle :unneeded
