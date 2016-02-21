770b29c976b39c2909c5d9f7aa4845fcc28f2bae
okampfer <okampfer@gmail.com>
2016-02-21 16:25:06 +0800

    Fix smartgit.

--- Formula/smartgit.rb
+++ Formula/smartgit.rb
@@ -2,7 +2,7 @@ class Smartgit < Formula
     desc "SmartGit is a Git client with support for GitHub Pull Requests+Comments, SVN and Mercurial."
     homepage "http://www.syntevo.com/smartgit/"
     version "7.1.1"
-    url "https://www.syntevo.com/downloads/smartgit/smartgit-linux-#{version.dots_to_underscores}.tar.gz"
+    url "https://www.syntevo.com/downloads/smartgit/smartgit-linux-7_1_1.tar.gz"
     sha256 "b963cdadf47c1a6f327d1496b813924141c3e81ffc90fa504f64262af438d352"
 
     bottle :unneeded
