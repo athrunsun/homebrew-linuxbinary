fd4f79a620d3b9958a9324634dc17a33eb99d5e8
okampfer <okampfer@gmail.com>
2016-02-21 15:00:55 +0800

    Fix android studio.

--- Formula/android-studio.rb
+++ Formula/android-studio.rb
@@ -2,7 +2,7 @@ class AndroidStudio < Formula
     desc "The official Android IDE."
     homepage "https://developer.android.com/sdk/"
     version "1.5.1.0_2456560"
-    url "https://dl.google.com/dl/android/studio/ide-zips/#{version.sub(%r{_.*}, '')}/android-studio-ide-141.#{version.sub(%r{.*_}, '')}-linux.zip"
+    url "https://dl.google.com/dl/android/studio/ide-zips/1.5.1.0/android-studio-ide-141.2456560-linux.zip"
     sha256 "466e01ac99c44685f218cf8dfc621fcc6b1d2c18256d97dbefa377d06410d05c"
 
     bottle :unneeded
