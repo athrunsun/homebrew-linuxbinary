9f418912dd09a4264952f90869ee94b98e30baf3
BruceSun <okampfer@outlook.com>
2016-04-21 03:18:40 +0000

    Upgrade datagrip to version 2016.1

--- Formula/datagrip.rb
+++ Formula/datagrip.rb
@@ -1,9 +1,9 @@
 class Datagrip < Formula
     desc "Your Swiss Army Knife for Databases and SQL."
     homepage "http://www.jetbrains.com/datagrip/"
-    version "1.0.3"
-    url "https://download.jetbrains.com/datagrip/datagrip-#{version}-custom-jdk-linux.tar.gz"
-    sha256 "996a9d6f3e4bb643ea5470673170e89a6330af1d19728785ba3ea1d7ad008081"
+    version "2016.1"
+    url "https://download.jetbrains.com/datagrip/datagrip-#{version}.tar.gz"
+    sha256 "9bd8a78ff5ad8344dbd111bf3a48a277f71fd8a8347d3fb9a1c09af4320eb9e3"
 
     bottle :unneeded
 
