fecd125301fd6825837dfb70f1380a8ccf4e1c70
BruceSun <okampfer@outlook.com>
2016-08-11 03:10:09 +0000

    Upgrade smartgit to version 7.1.4

--- Formula/smartgit.rb
+++ Formula/smartgit.rb
@@ -1,9 +1,9 @@
 class Smartgit < Formula
     desc "SmartGit is a Git client with support for GitHub Pull Requests+Comments, SVN and Mercurial."
     homepage "http://www.syntevo.com/smartgit/"
-    version "7.1.3"
-    url "https://www.syntevo.com/downloads/smartgit/smartgit-linux-7_1_3.tar.gz"
-    sha256 "823d47da1c03cb78a5d4e338621ccf0c3a11edc0de0774f954c3ac07a6689268"
+    version "7.1.4"
+    url "https://www.syntevo.com/downloads/smartgit/smartgit-linux-7_1_4.tar.gz"
+    sha256 "66e22d01689a8cd1b25accf397c89fdc0389c73df821ca45bf084370825e83ad"
 
     bottle :unneeded
 
