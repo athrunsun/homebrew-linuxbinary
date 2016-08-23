e89ef947bcd269ade787a326fd812cd21d9efab2
Joe Stanton <jsromejoe@gmail.com>
2016-08-23 11:17:06 -0400

    Update Webstorm version
    The 2016.2 version appears to have stopped working. 
    https://download.jetbrains.com/webstorm/WebStorm-2016.2.tar.gz gives a 403 error

--- Formula/webstorm.rb
+++ Formula/webstorm.rb
@@ -1,7 +1,7 @@
 class Webstorm < Formula
     desc "The smartest JavaScript IDE."
     homepage "http://www.jetbrains.com/webstorm/"
-    version "2016.2"
+    version "2016.2.2"
     url "https://download.jetbrains.com/webstorm/WebStorm-#{version}.tar.gz"
     sha256 "a3453ce89f6dfc55de1723b3e6119a71771a20a8c4ca4da60dcb1fd91e3700d9"
 
