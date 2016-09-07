245157f420fe78a676836b43481cec0cf8bc16a8
okampfer <okampfer@gmail.com>
2016-09-07 22:36:33 +0800

    Upgrade lantern to version 2.2.5.

--- Formula/lantern.rb
+++ Formula/lantern.rb
@@ -1,14 +1,14 @@
 class Lantern < Formula
     desc "Lantern is a free application that delivers fast, reliable and secure access to the open Internet."
     homepage "https://getlantern.org/"
-    version "2.1.2"
+    version "2.2.5"
 
     if MacOS.prefer_64_bit?
         url "https://github.com/getlantern/lantern/releases/download/#{version}/update_linux_amd64.bz2"
-        sha256 "45aec7d3bfe82f061bcb42172395eb8598994c3f3d3a269645de77f58e0f36fc"
+        sha256 "6520d25bb049d038f7e0dc9944eac75569592114f92a22836092353c810617bb"
     else
         url "https://github.com/getlantern/lantern/releases/download/#{version}/update_linux_386.bz2"
-        sha256 "a36308fcdf9d64ee93b6b61f7849cb184160743cf6054cd43fe3fccb24730ba1"
+        sha256 "da09c9650541402ceab990d73fd61f0589324f37055e4c159f7ad11760fdf1ab"
     end
 
     bottle :unneeded
