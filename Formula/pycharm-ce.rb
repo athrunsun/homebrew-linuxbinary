d85a9a218dd65ea9b9c057fdd0612f704109a2b1
BruceSun <okampfer@outlook.com>
2016-09-24 12:54:18 +0000

    Upgrade pycharm to version 2016.2.3

--- Formula/pycharm-ce.rb
+++ Formula/pycharm-ce.rb
@@ -1,9 +1,9 @@
 class PycharmCe < Formula
     desc "Python IDE for Professional Developers, community edition."
     homepage "http://www.jetbrains.com/pycharm/"
-    version "2016.2"
+    version "2016.2.3"
     url "https://download.jetbrains.com/python/pycharm-community-#{version}.tar.gz"
-    sha256 "334eaa740d963cf12bf2e5c2107909d16238b739c2afa017dc4587103eb60176"
+    sha256 "8ea07f6ff9f63a56cd6c779a3b1727157e177c2c4da8c056dec60b056e03f05a"
 
     bottle :unneeded
 
--- Formula/pycharm.rb
+++ Formula/pycharm.rb
@@ -1,9 +1,9 @@
 class Pycharm < Formula
     desc "Python IDE for Professional Developers, professional edition."
     homepage "http://www.jetbrains.com/pycharm/"
-    version "2016.2"
+    version "2016.2.3"
     url "https://download.jetbrains.com/python/pycharm-professional-#{version}.tar.gz"
-    sha256 "8ac7bdb58af1d3fec7b850fc158b4da72d33d140ad01daca0d3f0afac1c7aa14"
+    sha256 "a1eef156cc56eb3a36b437684b68b2e16f2575b5f1245a617eceb8352f6f4f5e"
 
     bottle :unneeded
 
