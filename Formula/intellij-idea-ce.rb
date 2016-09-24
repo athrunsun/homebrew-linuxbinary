1ae189cc535df912372176c477a3fba75ba830a8
BruceSun <okampfer@outlook.com>
2016-09-24 12:53:53 +0000

    Upgrade intellij to 2016.2.4

--- Formula/intellij-idea-ce.rb
+++ Formula/intellij-idea-ce.rb
@@ -1,9 +1,9 @@
 class IntellijIdeaCe < Formula
     desc "The Most Intelligent Java IDE, community edition."
     homepage "http://www.jetbrains.com/idea/"
-    version "2016.2"
+    version "2016.2.4"
     url "https://download.jetbrains.com/idea/ideaIC-#{version}.tar.gz"
-    sha256 "6270c2feae18e10a790d7dda0ab5fed929e353ef41cb016560f7878101259d98"
+    sha256 "bfd1bc75386d22bf9f0559b4894b547689383bd69c4038a1516a6e2c00f96742"
 
     bottle :unneeded
 
--- Formula/intellij-idea.rb
+++ Formula/intellij-idea.rb
@@ -1,9 +1,9 @@
 class IntellijIdea < Formula
     desc "The Most Intelligent Java IDE, ultimate edition."
     homepage "http://www.jetbrains.com/idea/"
-    version "2016.2"
+    version "2016.2.4"
     url "https://download.jetbrains.com/idea/ideaIU-#{version}.tar.gz"
-    sha256 "647ea2b7c1954012ca2e1e027b59ad7fa6cf24dee2dcc22c135155c60cc41182"
+    sha256 "f940fb9fa356e28351dd99cf2612c967d173e4413b66c0452ee8fa651b64b698"
 
     bottle :unneeded
 
