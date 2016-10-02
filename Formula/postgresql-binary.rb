bea89a74f2f9239192154a19b069786524c29b6d
okampfer <okampfer@gmail.com>
2016-10-02 23:29:33 +0800

    Upgrade postgresql binary to version 9.6.0-1

--- Formula/postgresql-binary.rb
+++ Formula/postgresql-binary.rb
@@ -1,15 +1,10 @@
 class PostgresqlBinary < Formula
     desc "PostgreSQL binary installation from EnterpriseDB."
     homepage "http://www.enterprisedb.com/"
-    version "9.5.1-1"
+    version "9.6.0-1"
 
-    if MacOS.prefer_64_bit?
-        url "http://get.enterprisedb.com/postgresql/postgresql-#{version}-linux-x64-binaries.tar.gz"
-        sha256 "1d85f85cd9e8236038b90ce5c078d22bd458fe8f924b46c4d341e3344753c309"
-    else
-        url "http://get.enterprisedb.com/postgresql/postgresql-#{version}-linux-binaries.tar.gz"
-        sha256 "630e6dafedf96e2dc61f6c1e584fdc0debcf9f099ebd5ec05ffbd379d41cec61"
-    end
+    url "http://get.enterprisedb.com/postgresql/postgresql-#{version}-linux-binaries.tar.gz"
+    sha256 "4da4eb3c5f0e2baade3ca24241796dbecfe728019238944833793b05fe245e84"
 
     bottle :unneeded
     conflicts_with "postgresql"
