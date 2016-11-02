432864fb4cb661492bcc664055554df7ae5f7bed
romejoe <jsromejoe@gmail.com>
2016-11-02 18:01:51 -0400

    Updated JetBrains products and added rubymine and clion

--- Formula/webstorm.rb
+++ Formula/webstorm.rb
@@ -1,9 +1,9 @@
 class Webstorm < Formula
     desc "The smartest JavaScript IDE."
     homepage "http://www.jetbrains.com/webstorm/"
-    version "2016.2.2"
+    version "2016.2.4"
     url "https://download.jetbrains.com/webstorm/WebStorm-#{version}.tar.gz"
-    sha256 "06af83ee181b37ae0a1108b4b88ae9a8d10f153796f3c59c879a4ead24dc4e58"
+    sha256 "6fe7fe6591260a95a588038838bb1d44e72175118b7a848d588d2ddb09a1c1c0"
 
     bottle :unneeded
 
