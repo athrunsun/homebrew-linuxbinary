cdee5bb3162a2f168765655839a62eb8384cac5b
okampfer <okampfer@gmail.com>
2016-12-08 23:00:07 +0800

    Add new formula resilio-sync. Update youtube-dl-binary to download tar ball.

--- Formula/youtube-dl-binary.rb
+++ Formula/youtube-dl-binary.rb
@@ -1,14 +1,13 @@
 class YoutubeDlBinary < Formula
     desc "Download videos from YouTube (and more sites)."
     homepage "https://rg3.github.io/youtube-dl/"
-    url "https://github.com/rg3/youtube-dl/releases/download/2016.11.22/youtube-dl"
-    sha256 "e4eda57345514b970da69560360cbacd808821a4e91b735b773e6170d6668945"
+    url "https://yt-dl.org/downloads/2016.12.01/youtube-dl-2016.12.01.tar.gz"
+    sha256 "ddff281a251c7a40bf8c28afa1df63e9a64ff61b6b8097535fbbe587e8ccaef7"
     bottle :unneeded
     conflicts_with "youtube-dl"
 
     def install
         libexec.install Dir["*"]
-        chmod(0755, "#{libexec}/youtube-dl")
-        bin.install_symlink("#{libexec}/youtube-dl")
+        bin.install_symlink("#{libexec}/bin/youtube-dl")
     end
 end
