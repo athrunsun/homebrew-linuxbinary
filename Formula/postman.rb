fbd46e9f6c41757a43f8989a7d9bae067e98eddf
Bruce Sun <bruce.sun@tubemogul.com>
2016-12-23 09:15:40 +0800

    Update postman to use static downloading url with version number according to https://github.com/postmanlabs/postman-app-support/issues/2461.

--- Formula/postman.rb
+++ Formula/postman.rb
@@ -1,14 +1,14 @@
 class Postman < Formula
     desc "Postman helps you develop APIs faster."
     homepage "https://www.getpostman.com/"
-    version "4.8.3"
+    version "4.9.1"
 
     if MacOS.prefer_64_bit?
-        url "https://dl.pstmn.io/download/latest/linux?arch=64"
-        sha256 "2661ff3edd0fb9cfaa00b952ac0640ac29b9c850260bb0f5ec000871446d554c"
+        url "https://dl.pstmn.io/download/version/#{version}/linux64"
+        sha256 "f6c2ca7db3c30393b46f7b614b10e904e3951c5e354b317b9dba33ece51aa96e"
     else
-        url "https://dl.pstmn.io/download/latest/linux?arch=32"
-        sha256 "19271ede9fa1d4ec675df51e62f518336bc29be33ca9a35c6ed81b37e0f91f97"
+        url "https://dl.pstmn.io/download/version/#{version}/linux32"
+        sha256 "b7c3dcf44f0a0727084af1dc0b8e69677fd078e7dd0b95af5386d2e53f9d6dd6"
     end
 
     bottle :unneeded
