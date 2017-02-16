c1f033cce984ce8c7a59aebd90a628cafe803fb7
okampfer <okampfer@gmail.com>
2017-02-16 21:36:08 +0800

    Upgrade smartgit to version 17.0.2

--- Formula/smartgit.rb
+++ Formula/smartgit.rb
@@ -1,9 +1,9 @@
 class Smartgit < Formula
     desc "SmartGit is a Git client with support for GitHub Pull Requests+Comments, SVN and Mercurial."
     homepage "http://www.syntevo.com/smartgit/"
-    version "8.0.3"
-    url "http://www.syntevo.com/static/smart/download/smartgit/smartgit-linux-8_0_3.tar.gz"
-    sha256 "4308cfaf07a3e9c95267c159472871071a0831b9d81a81b3e35388daca931dbe"
+    version "17.0.2"
+    url "http://www.syntevo.com/static/smart/download/smartgit/smartgit-linux-17_0_2.tar.gz"
+    sha256 "7ceb72627230d428eae67e86df3f1408e873b944f066bc277e770597c071ff16"
 
     bottle :unneeded
 
