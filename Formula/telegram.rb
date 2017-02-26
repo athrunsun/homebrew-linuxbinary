c49f337ab28d4f918e1292f80e5c79bc08fd55b3
okampfer <okampfer@gmail.com>
2017-02-26 10:25:35 +0800

    Upgrade telegram to version 1.0.14

--- Formula/telegram.rb
+++ Formula/telegram.rb
@@ -1,14 +1,14 @@
 class Telegram < Formula
     desc "Telegram, a new era of messaging."
     homepage "https://telegram.org/"
-    version "0.10.19"
+    version "1.0.14"
 
     if MacOS.prefer_64_bit?
         url "https://updates.tdesktop.com/tlinux/tsetup.#{version}.tar.xz"
-        sha256 "586a6d98d8ee412581a73e501287f4bc972fa966f34e62c245878e6ef183042b"
+        sha256 "88dcffe9fe21855dbf335ad6b94d5a998773d4339b5ca42ce53ae01a2835f448"
     else
         url "https://updates.tdesktop.com/tlinux32/tsetup32.#{version}.tar.xz"
-        sha256 "b0f9c9ff51f8caf67f549960b2c4fde3d2d683ac8e77b3ed1c2d4c9e7f3283b3"
+        sha256 "5902d7164073d61056629490fef041c84083d185e8aa5edf8bffacb52bdb5856"
     end
 
     bottle :unneeded
