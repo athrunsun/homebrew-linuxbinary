3959cc074c5a70a3ab5d0fddde402b56e4d60743
okampfer <okampfer@gmail.com>
2017-03-12 17:25:29 +0800

    Fix wrong downloading url for dbeaver-ce.

--- Formula/dbeaver-ce.rb
+++ Formula/dbeaver-ce.rb
@@ -4,10 +4,10 @@ class DbeaverCe < Formula
     version "4.0.2"
 
     if MacOS.prefer_64_bit?
-        url "http://dbeaver.jkiss.org/files/dbeaver-ee-latest-linux.gtk.x86_64.tar.gz"
+        url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-linux.gtk.x86_64.tar.gz"
         sha256 "9122b527436022266f2e951a7bca845995eb7f1a1e7c89c30ae634462a11ae4c"
     else
-        url "http://dbeaver.jkiss.org/files/dbeaver-ee-latest-linux.gtk.x86.tar.gz"
+        url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-linux.gtk.x86.tar.gz"
         sha256 "f1285f4630953514c10923d6e4799cc7eec2e049e57dd8263ce88d10c0d99431"
     end
 
@@ -15,6 +15,6 @@ class DbeaverCe < Formula
 
     def install
         libexec.install Dir["*"]
-        bin.install_symlink("#{libexec}/dbeaver")
+        bin.install_symlink({"#{libexec}/dbeaver" => "dbeaverc"})
     end
 end
--- Formula/dbeaver-ee.rb
+++ Formula/dbeaver-ee.rb
@@ -4,10 +4,10 @@ class DbeaverEe < Formula
     version "3.8.5"
 
     if MacOS.prefer_64_bit?
-        url "http://dbeaver.jkiss.org/files/dbeaver-ee-latest-linux.gtk.x86_64.tar.gz"
+        url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ee-#{version}-linux.gtk.x86_64.tar.gz"
         sha256 "72c34dfc11918a3e758eeda699175cdabc903fd69874346e9f225af93c32744a"
     else
-        url "http://dbeaver.jkiss.org/files/dbeaver-ee-latest-linux.gtk.x86.tar.gz"
+        url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ee-#{version}-linux.gtk.x86.tar.gz"
         sha256 "298ff1244b02ac298b36f355a0c40a81137a7d6d4b874325640709a77a1db81b"
     end
 
