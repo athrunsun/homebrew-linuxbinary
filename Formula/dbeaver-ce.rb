4fea1ac72d673182bc2e413c5ebf0ceb6b000074
okampfer <okampfer@gmail.com>
2017-03-12 16:46:50 +0800

    Upgrade dbeaver-ee to version 3.8.5, add dbeaver-ce version 4.0.2

---
+++ Formula/dbeaver-ce.rb
@@ -0,0 +1,20 @@
+class DbeaverCe < Formula
+    desc "Free multi-platform database tool for developers, SQL programmers, database administrators and analysts. Supports any database which has JDBC driver (which basically means - ANY database)."
+    homepage "http://dbeaver.jkiss.org/"
+    version "4.0.2"
+
+    if MacOS.prefer_64_bit?
+        url "http://dbeaver.jkiss.org/files/dbeaver-ee-latest-linux.gtk.x86_64.tar.gz"
+        sha256 "9122b527436022266f2e951a7bca845995eb7f1a1e7c89c30ae634462a11ae4c"
+    else
+        url "http://dbeaver.jkiss.org/files/dbeaver-ee-latest-linux.gtk.x86.tar.gz"
+        sha256 "f1285f4630953514c10923d6e4799cc7eec2e049e57dd8263ce88d10c0d99431"
+    end
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/dbeaver")
+    end
+end
--- Formula/dbeaver-ee.rb
+++ Formula/dbeaver-ee.rb
@@ -1,14 +1,14 @@
 class DbeaverEe < Formula
     desc "Free multi-platform database tool for developers, SQL programmers, database administrators and analysts. Supports all popular databases: MySQL, PostgreSQL, SQLite, Oracle, DB2, SQL Server, Sybase, MongoDB, etc."
     homepage "http://dbeaver.jkiss.org/"
-    version "3.6"
+    version "3.8.5"
 
     if MacOS.prefer_64_bit?
         url "http://dbeaver.jkiss.org/files/dbeaver-ee-latest-linux.gtk.x86_64.tar.gz"
-        sha256 "f60966dd5caa067edd00baa672d68c0f2a54c95dc44154782c072779bdbf24fc"
+        sha256 "72c34dfc11918a3e758eeda699175cdabc903fd69874346e9f225af93c32744a"
     else
         url "http://dbeaver.jkiss.org/files/dbeaver-ee-latest-linux.gtk.x86.tar.gz"
-        sha256 "3e823daea200846a2e3ed355291d4a63e0730e2f302ea1fe811176fafe514b62"
+        sha256 "298ff1244b02ac298b36f355a0c40a81137a7d6d4b874325640709a77a1db81b"
     end
 
     bottle :unneeded
