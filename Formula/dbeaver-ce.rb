b97c906248414837adb02212b49a3fa7a45bbc11
okampfer <okampfer@gmail.com>
2017-03-12 17:31:21 +0800

    Fix wrong sha256 for dbeaver-ce.

--- Formula/dbeaver-ce.rb
+++ Formula/dbeaver-ce.rb
@@ -5,7 +5,7 @@ class DbeaverCe < Formula
 
     if MacOS.prefer_64_bit?
         url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-linux.gtk.x86_64.tar.gz"
-        sha256 "9122b527436022266f2e951a7bca845995eb7f1a1e7c89c30ae634462a11ae4c"
+        sha256 "2fe19877bdccbc5e89fccb727c5baeb74aa7bd8b1d854736a7d939bcd2b57d42"
     else
         url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-linux.gtk.x86.tar.gz"
         sha256 "f1285f4630953514c10923d6e4799cc7eec2e049e57dd8263ce88d10c0d99431"
