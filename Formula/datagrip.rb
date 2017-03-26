c4f0e5ea5fde90cb1a2bd827be200ff605b95596
okampfer <okampfer@gmail.com>
2017-03-26 08:55:04 +0800

    Upgrade datagrip to version 2017.1

--- Formula/datagrip.rb
+++ Formula/datagrip.rb
@@ -1,9 +1,9 @@
 class Datagrip < Formula
     desc "Your Swiss Army Knife for Databases and SQL."
     homepage "http://www.jetbrains.com/datagrip/"
-    version "2016.3.3"
+    version "2017.1"
     url "https://download.jetbrains.com/datagrip/datagrip-#{version}.tar.gz"
-    sha256 "b7673739d1253d90978f3571e82a8dbbdc04b9262976fc41030d38fd3a0ee593"
+    sha256 "91ee6a1e43d75a45ae51829835e457da85262410d89e617324d0239ba5625dfa"
 
     bottle :unneeded
 
