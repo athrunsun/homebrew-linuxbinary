0d51a10b79fb9cc2eeac027c7f0a79cfb66cbf4f
Matthew Andersen <matthew@barelystable.com>
2017-06-17 00:43:29 -0700

    Update GitKraken to 2.6.0

--- Formula/git-kraken.rb
+++ Formula/git-kraken.rb
@@ -1,9 +1,9 @@
 class GitKraken < Formula
     desc "The downright luxurious Git client, for Windows, Mac & Linux."
     homepage "https://www.gitkraken.com/"
-    version "1.9.1"
+    version "2.6.0"
     url "https://release.gitkraken.com/linux/gitkraken-amd64.tar.gz"
-    sha256 "71bc394150053bb83e1ec5497952675f416be044de500759deecbe081a21d80e"
+    sha256 "0e51841e518db6f8ad831eba23caad5355cfa23b8cfe742e53a9283d816bcb9d"
 
     bottle :unneeded
 
