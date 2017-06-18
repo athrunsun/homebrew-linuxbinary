6c7726d384670a02fe249e31871f1b47e1763ff2
okampfer <okampfer@gmail.com>
2017-06-18 09:16:58 +0800

    Upgrade electronic-wechat to version 2.0

--- Formula/electronic-wechat.rb
+++ Formula/electronic-wechat.rb
@@ -1,9 +1,9 @@
 class ElectronicWechat < Formula
     desc "The Most Intelligent Java IDE, community editionA better WeChat on macOS and Linux. Built with Electron by Zhongyi Tong."
     homepage "https://github.com/geeeeeeeeek/electronic-wechat"
-    version "1.4.0"
-    url "https://github.com/geeeeeeeeek/electronic-wechat/releases/download/v1.4.0/linux-x64.tar.gz"
-    sha256 "5f34e478db1153ea8aba8816f8aec59b3a1b5256951fbe26bfd476412dcea5cf"
+    version "2.0"
+    url "https://github.com/geeeeeeeeek/electronic-wechat/releases/download/V#{version}/linux-x64.tar.gz"
+    sha256 "e4922c16c8a283597698ffdd827347398cee80cb93580b8b3aa8d618e03b355c"
 
     bottle :unneeded
 
