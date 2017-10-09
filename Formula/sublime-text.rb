7a51895955999cff9f21502a9d96f62b8dca0cf6
Bruce Sun <bruce.sun@tubemogul.com>
2017-10-09 09:38:59 +0800

    Upgrade sublime text to version 3143.

--- Formula/sublime-text.rb
+++ Formula/sublime-text.rb
@@ -1,14 +1,14 @@
 class SublimeText < Formula
     desc "Sublime Text is a sophisticated text editor for code, markup and prose. This formula will install from generic tarball."
     homepage "https://www.sublimetext.com/"
-    version "3126"
+    version "3143"
 
     if MacOS.prefer_64_bit?
         url "https://download.sublimetext.com/sublime_text_3_build_#{version}_x64.tar.bz2"
-        sha256 "18db132e9a305fa3129014b608628e06f9442f48d09cfe933b3b1a84dd18727a"
+        sha256 "9ce120c4f28b239d3b3860ee672d9d87e1397a4c08ee6c4e62fd6e261a296519"
     else
         url "https://download.sublimetext.com/sublime_text_3_build_#{version}_x32.tar.bz2"
-        sha256 "92ffefa470f0777897ed0dfb7c1635426105271da9b5affbe8c1e82039718e29"
+        sha256 "b1ecc4b70d66b9236b876f1913c4094b6dd51436e45c74883ba70a1939e9f735"
     end
 
     bottle :unneeded
