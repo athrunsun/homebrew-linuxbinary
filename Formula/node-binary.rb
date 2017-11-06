a41ac9f37d7c4d20621fa7f7f0c2e8968a0ad84a
Bruce Sun <bruce.sun@tubemogul.com>
2017-11-06 13:38:28 +0800

    Upgrade node-binary to version 9.0.0, rename node-binary-lts to node-lts-binary and upgrade to version 8.9.0.

--- Formula/node-binary.rb
+++ Formula/node-binary.rb
@@ -1,7 +1,7 @@
 class NodeBinary < Formula
     desc "Platform built on the V8 JavaScript runtime to build network applications, binaries installation. This is the current version with latest features."
     homepage "https://nodejs.org/"
-    version "7.3.0"
+    version "9.0.0"
 
     option "with-taobao-mirror", "Download tarball from https://npm.taobao.org/mirrors/node/, use this option if you're in mainland China for a faster downloading speed."
 
@@ -12,7 +12,7 @@ class NodeBinary < Formula
             url "https://nodejs.org/dist/v#{version}/node-v#{version}-linux-x64.tar.xz"
         end
 
-        sha256 "e5caa2040084015198cab97181aa450c687a19cbf76a09cc81e8368dad81fd8c"
+        sha256 "8313d2f6d69dbea4cb860803a156f093041cbe352a843c06f9f05cab1f30e9cc"
     else
         if build.with? "taobao-mirror"
             url "https://npm.taobao.org/mirrors/node/v#{version}/node-v#{version}-linux-x86.tar.xz"
@@ -20,11 +20,11 @@ class NodeBinary < Formula
             url "https://nodejs.org/dist/v#{version}/node-v#{version}-linux-x86.tar.xz"
         end
 
-        sha256 "90edc1e88c07ed63c1fb5acc86f0c9280821f2371d64d73c05cb6e3b41df10a8"
+        sha256 "1d4f2e0db1efa0f9850f272327243ee17aa5bef2e46807bb1adf2e5ba2b56c59"
     end
 
     bottle :unneeded
-    conflicts_with "node", "node-binary-lts"
+    conflicts_with "node", "node-lts-binary"
 
     def install
         libexec.install Dir["*"]
--- Formula/node-lts-binary.rb
+++ Formula/node-lts-binary.rb
@@ -1,7 +1,7 @@
-class NodeBinaryLts < Formula
+class NodeLtsBinary < Formula
     desc "Platform built on the V8 JavaScript runtime to build network applications, binaries installation. This is the LTS version and is recommended for most users."
     homepage "https://nodejs.org/"
-    version "6.9.2"
+    version "8.9.0"
 
     option "with-taobao-mirror", "Download tarball from https://npm.taobao.org/mirrors/node/, use this option if you're in mainland China for a faster downloading speed."
 
@@ -12,7 +12,7 @@ class NodeBinaryLts < Formula
             url "https://nodejs.org/dist/v#{version}/node-v#{version}-linux-x64.tar.xz"
         end
 
-        sha256 "da766edda11cc38eefb1ce29683f248f40c997c0ee2e06903b01429b4c94b71a"
+        sha256 "e92b91fa473f9ad805a1241907b6f1bd3f8ceac8426a8b4cb05428e62e243bdd"
     else
         if build.with? "taobao-mirror"
             url "https://npm.taobao.org/mirrors/node/v#{version}/node-v#{version}-linux-x86.tar.xz"
@@ -20,7 +20,7 @@ class NodeBinaryLts < Formula
             url "https://nodejs.org/dist/v#{version}/node-v#{version}-linux-x86.tar.xz"
         end
 
-        sha256 "9dae6ddbafcefd271c3df6e01633422dc7495479269fb1358e4c540929ef8835"
+        sha256 "7bd958d48222b04f65fbb109334b75bec927ada0a223b6aa64c44c3e5d6d6d2b"
     end
 
     bottle :unneeded
