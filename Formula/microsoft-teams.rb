f860b80935009f7f320729471768585422f4b2fe
j <74ef54cf@opayq.com>
2017-12-14 11:05:55 -0800

    update teams 0.0.5

--- Formula/microsoft-teams.rb
+++ Formula/microsoft-teams.rb
@@ -3,9 +3,9 @@ require "language/node"
 class MicrosoftTeams < Formula
   desc "Unofficial Microsoft Teams for Linux client"
   homepage "https://github.com/ivelkov/teams-for-linux"
-  version "0.0.4"
+  version "0.0.5"
   url "https://github.com/ivelkov/teams-for-linux/releases/download/v#{version}/teams-for-linux-#{version}.tar.gz"
-  sha256 "65fa915740bccce609b38d8bb2e15063ebf4c34d50948f2a7b634129e402a183"
+  sha256 "9b7b14e65b18f9d0396d2367999d67cd0a87412169407e7350e2b463a9f9ef5d"
   head "https://github.com/ivelkov/teams-for-linux.git"
 
   bottle :unneeded
