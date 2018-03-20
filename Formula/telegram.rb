64a5e0986a1101215ef37de169d1270dfd9dfc71
Bruce Sun <brucesun@adobe.com>
2018-03-20 10:14:13 +0800

    Update formula telegram a little bit, add github downloading urls.

--- Formula/telegram.rb
+++ Formula/telegram.rb
@@ -1,13 +1,25 @@
 class Telegram < Formula
     desc "Telegram, a new era of messaging."
-    homepage "https://telegram.org/"
+    homepage "https://telegram.org/ https://github.com/telegramdesktop/tdesktop"
     version "1.2.6"
 
+    option "with-mirror", "Download installation archive from https://updates.tdesktop.com"
+
     if MacOS.prefer_64_bit?
-        url "https://updates.tdesktop.com/tlinux/tsetup.#{version}.tar.xz"
+        if build.with? "mirror"
+            url "https://updates.tdesktop.com/tlinux/tsetup.#{version}.tar.xz"
+        else
+            url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup.#{version}.tar.xz"
+        end
+
         sha256 "d132a5710765dbd4120c13864ce81a546ea3b4fda14b9736ecc2310bd0513d3d"
     else
-        url "https://updates.tdesktop.com/tlinux32/tsetup32.#{version}.tar.xz"
+        if build.with? "mirror"
+            url "https://updates.tdesktop.com/tlinux32/tsetup32.#{version}.tar.xz"
+        else
+            url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup32.#{version}.tar.xz"
+        end
+
         sha256 "54dda2b1d0b961d0e0dc7fa70dcf4e54cb2836ce4c887526fb366044928576fc"
     end
 
@@ -16,5 +28,10 @@ class Telegram < Formula
     def install
         libexec.install Dir["*"]
         bin.install_symlink("#{libexec}/Telegram" => "telegram")
+    end
+
+    def caveats; <<~EOS
+        Executable linked as "telegram".
+        EOS
     end
 end
