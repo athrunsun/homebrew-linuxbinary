7d6a6c229f9a63fc8604f7fd21ecee292529de59
okampfer <okampfer@gmail.com>
2018-04-18 10:34:05 +0800

    Upgrade android studio to version 3.1.1.0

--- Formula/android-studio.rb
+++ Formula/android-studio.rb
@@ -1,14 +1,19 @@
 class AndroidStudio < Formula
     desc "The official Android IDE."
-    homepage "https://developer.android.com/sdk/"
-    version "1.5.1.0"
-    url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-141.2456560-linux.zip"
-    sha256 "466e01ac99c44685f218cf8dfc621fcc6b1d2c18256d97dbefa377d06410d05c"
+    homepage "https://developer.android.com/studio/"
+    version "3.1.1.0"
+    url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-173.4697961-linux.zip"
+    sha256 "08539019cda4f9228f6e08d9fda8933a71ecae29eac75f8d31e743816315c076"
 
     bottle :unneeded
 
     def install
         libexec.install Dir["*"]
         bin.install_symlink("#{libexec}/bin/studio.sh" => "android_studio")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "android_studio".
+        EOS
     end
 end
