1f35605e0d7f73a7594c87b39910ad1510917de7
Santiago Alessandri <san.lt.ss@gmail.com>
2018-04-18 14:37:52 -0300

    Update GitKraken to version 3.5.1

--- Formula/git-kraken.rb
+++ Formula/git-kraken.rb
@@ -1,9 +1,9 @@
 class GitKraken < Formula
     desc "The downright luxurious Git client, for Windows, Mac & Linux."
     homepage "https://www.gitkraken.com/"
-    version "2.6.0"
+    version "3.5.1"
     url "https://release.gitkraken.com/linux/gitkraken-amd64.tar.gz"
-    sha256 "0e51841e518db6f8ad831eba23caad5355cfa23b8cfe742e53a9283d816bcb9d"
+    sha256 "f12cb1c4a6dfd18310ac5022bcf53fc9d610b43c66e111d17faea71f83fdc4b5"
 
     bottle :unneeded
 
