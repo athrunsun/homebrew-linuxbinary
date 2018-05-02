430b937f274e55bf34dfc63b8a0cb86aaa12a05e
okampfer <okampfer@gmail.com>
2018-05-02 22:49:54 +0800

    Add dbdeployer for linux and osx, version 1.4.0

---
+++ Formula/dbdeployer.rb
@@ -0,0 +1,20 @@
+class Dbdeployer < Formula
+    desc "A tool that deploys MySQL database servers easily."
+    homepage "https://github.com/datacharmer/dbdeployer"
+    version "1.4.0"
+
+    url "https://github.com/datacharmer/dbdeployer/releases/download/#{version}/dbdeployer-#{version}.linux.tar.gz"
+    sha256 "5065d1e7ee2ff89f2d568bf5075689a640915bb108a3559d82f405d264787d35"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/dbdeployer-#{version}.linux" => "dbdeployer")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "dbdeployer".
+        EOS
+    end
+end
