ff8acfc58ad23a6a13a5928625f8ba50635598c2
okampfer <okampfer@gmail.com>
2018-05-09 16:13:28 +0800

    Upgrade sublime-text to version 3170

--- Formula/sublime-text.rb
+++ Formula/sublime-text.rb
@@ -1,14 +1,14 @@
 class SublimeText < Formula
     desc "Sublime Text is a sophisticated text editor for code, markup and prose. This formula will install from tarball for generic linux platform."
     homepage "https://www.sublimetext.com/"
-    version "3143"
+    version "3170"
 
     if MacOS.prefer_64_bit?
         url "https://download.sublimetext.com/sublime_text_3_build_#{version}_x64.tar.bz2"
-        sha256 "9ce120c4f28b239d3b3860ee672d9d87e1397a4c08ee6c4e62fd6e261a296519"
+        sha256 "fdcfc110626e89c5c40a6c38b700a40f8d608c2cdd1cd4fef63263004096dfea"
     else
         url "https://download.sublimetext.com/sublime_text_3_build_#{version}_x32.tar.bz2"
-        sha256 "b1ecc4b70d66b9236b876f1913c4094b6dd51436e45c74883ba70a1939e9f735"
+        sha256 "efbb5095995f9acc7df620325d52070fb4fd04a086c66082527b5f8bab489412"
     end
 
     bottle :unneeded
