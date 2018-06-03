a2a4d90b20a2067cfc3f4169685e47a8a5e8723f
okampfer <okampfer@gmail.com>
2018-06-03 22:58:10 +0800

    Add new formula mancy, version 3.2.0

---
+++ Formula/mancy.rb
@@ -0,0 +1,19 @@
+class Mancy < Formula
+    desc "Electron based NodeJS REPL."
+    homepage "https://github.com/princejwesley/Mancy"
+    version "3.2.0"
+    url "https://github.com/princejwesley/Mancy/releases/download/v#{version}/Mancy-linux-x64.zip"
+    sha256 "7b8143957bf08e7a7e4278e220ffa75db91c2d6932cec9170df5b236d7e09e7a"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink({"#{libexec}/Mancy" => "mancy"})
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "mancy".
+        EOS
+    end
+end
