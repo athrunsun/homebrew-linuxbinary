21952db633bc9e7864a6ac80bb4c13dd7dedd729
okampfer <okampfer@gmail.com>
2018-06-08 22:51:05 +0800

    Upgrade atom to version 1.27.2

--- Formula/atom.rb
+++ Formula/atom.rb
@@ -1,7 +1,7 @@
 class Atom < Formula
     desc "A hackable text editor for the 21st Century."
-    homepage "https://github.com/atom/atom"
-    version "1.23.3"
+    homepage "https://github.com/atom/atom  https://atom.io/"
+    version "1.27.2"
 
     option "with-taobao-mirror", "Download tarball from https://npm.taobao.org/mirrors/atom/, use this option if you're in mainland China for a faster downloading speed."
 
@@ -11,7 +11,7 @@ class Atom < Formula
         url "https://github.com/atom/atom/releases/download/v#{version}/atom-amd64.tar.gz"
     end
 
-    sha256 "2d22c5dd1fc5a991fd7a4ca6c4d64a9df8204b430af1a4e5b9227b5423068f75"
+    sha256 "1288171ef2f5b11704fbb179e9482e8b98d162d9b43ef865e10abac9528730c2"
 
     bottle :unneeded
 
