237b4959d5455cea74b7ee288efa634165c97f67
okampfer <okampfer@gmail.com>
2018-06-18 23:59:12 +0800

    Upgrade smartgit to version 18.1.3

--- Formula/smartgit.rb
+++ Formula/smartgit.rb
@@ -1,14 +1,19 @@
 class Smartgit < Formula
     desc "SmartGit is a Git client with support for GitHub Pull Requests+Comments, SVN and Mercurial."
     homepage "http://www.syntevo.com/smartgit/"
-    version "17.0.2"
-    url "http://www.syntevo.com/static/smart/download/smartgit/smartgit-linux-17_0_2.tar.gz"
-    sha256 "7ceb72627230d428eae67e86df3f1408e873b944f066bc277e770597c071ff16"
+    version "18.1.3"
+    url "https://www.syntevo.com/downloads/smartgit/smartgit-linux-18_1_3.tar.gz"
+    sha256 "ad12334c7608c9bcd515551caaf8711a4ebb090903b8874afdaccd9d36da74e7"
 
     bottle :unneeded
 
     def install
         libexec.install Dir["*"]
         bin.install_symlink("#{libexec}/bin/smartgit.sh" => "smartgit")
+    end
+
+    def caveats; <<~EOS
+        Executable linked as "smartgit".
+        EOS
     end
 end
