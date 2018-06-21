0b25e9bf951504759d76499f8f5b462030ba47f3
okampfer <okampfer@gmail.com>
2018-06-21 20:11:27 +0800

    Upgrade youtube-dl-binary to version 2018.06.19

--- Formula/youtube-dl-binary.rb
+++ Formula/youtube-dl-binary.rb
@@ -1,14 +1,19 @@
 class YoutubeDlBinary < Formula
     desc "Download videos from YouTube (and more sites)."
     homepage "https://rg3.github.io/youtube-dl/"
-    version "2017.01.28"
+    version "2018.06.19"
     url "https://yt-dl.org/downloads/#{version}/youtube-dl-#{version}.tar.gz"
-    sha256 "3ca165456799a9a60c875caed37e70c8fe3279326f2715837fcdc4304c64be99"
+    sha256 "e68ff7b344d24ecc7714b6c91ddbb833c438ac3e654d9f8fbdb29e4c10ab427b"
     bottle :unneeded
     conflicts_with "youtube-dl"
 
     def install
         libexec.install Dir["*"]
         bin.install_symlink("#{libexec}/bin/youtube-dl")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "youtube-dl".
+        EOS
     end
 end
