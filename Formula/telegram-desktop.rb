47870aa1cc7d09814965a243528fa20a9ee438b1
okampfer <okampfer@gmail.com>
2018-07-07 09:42:19 +0800

    Upgrade telegram to version 1.3.9

--- Formula/telegram-desktop.rb
+++ Formula/telegram-desktop.rb
@@ -1,26 +1,26 @@
 class TelegramDesktop < Formula
     desc "Telegram, a new era of messaging."
     homepage "https://github.com/telegramdesktop/tdesktop https://telegram.org/"
-    version "1.2.6"
+    version "1.3.9"
 
-    option "with-mirror", "Download installation archive from https://updates.tdesktop.com"
+    option "with-github", "Download installation archive from https://github.com/telegramdesktop/tdesktop"
 
     if MacOS.prefer_64_bit?
-        if build.with? "mirror"
-            url "https://updates.tdesktop.com/tlinux/tsetup.#{version}.tar.xz"
-        else
+        if build.with? "github"
             url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup.#{version}.tar.xz"
-        end
-
-        sha256 "d132a5710765dbd4120c13864ce81a546ea3b4fda14b9736ecc2310bd0513d3d"
-    else
-        if build.with? "mirror"
-            url "https://updates.tdesktop.com/tlinux32/tsetup32.#{version}.tar.xz"
         else
-            url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup32.#{version}.tar.xz"
+            url "https://updates.tdesktop.com/tlinux/tsetup.#{version}.tar.xz"
         end
 
-        sha256 "54dda2b1d0b961d0e0dc7fa70dcf4e54cb2836ce4c887526fb366044928576fc"
+        sha256 "693e25ca19ba8fc80ed3ea112d7287beb96fe057c1bf1c8c58c40191dc2bd9f9"
+    else
+        if build.with? "github"
+            url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup32.#{version}.tar.xz"
+        else
+            url "https://updates.tdesktop.com/tlinux32/tsetup32.#{version}.tar.xz"
+        end
+
+        sha256 "e37fa31ad99aff73cfd00cb40e095f4b90cffe42412c812a72f8672f5db78df0"
     end
 
     bottle :unneeded
@@ -28,10 +28,12 @@ class TelegramDesktop < Formula
     def install
         libexec.install Dir["*"]
         bin.install_symlink("#{libexec}/Telegram" => "telegram")
+        bin.install_symlink("#{libexec}/Updater" => "Updater")
     end
 
     def caveats; <<~EOS
         Executable linked as "telegram".
+        Auto updater is linked as "Updater".
         EOS
     end
 end
