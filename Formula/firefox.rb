1a4f1596b8aa75598abcb88dcd392a16a0acfc16
Caleb Predmore <cpredmore@tableau.com>
2018-07-11 08:25:51 -0700

    Update Firefox to 61.0.1

--- Formula/firefox.rb
+++ Formula/firefox.rb
@@ -1,14 +1,14 @@
 class Firefox < Formula
     desc 'The 100% fresh, free-range, ethical browser, with more speed, privacy and freedom. This is the stable version, in en-US.'
     homepage "https://www.mozilla.org/en-US/firefox/all/"
-    version "57.0.1"
+    version "61.0.1"
 
     if MacOS.prefer_64_bit?
         url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2"
-        sha256 "c232cd66ecebae6dcb0e25dbdd0901d238ea218ab8256adf1afacc1fe4a1941c"
+        sha256 "e59fc49056d03c9b614e8131551f9d1979a84459b9896ed42c04a3fd9dad2425"
     else
         url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-i686/en-US/firefox-#{version}.tar.bz2"
-        sha256 "1f749be0e9591606ac1656ef65afe302cc3afc487a779b02871098f2702bb079"
+        sha256 "3fcd3faf06280dc54bc4544a13d10d40f81d6ef5235624116e846c65000aad13"
     end
 
     conflicts_with "firefox-zhcn"
