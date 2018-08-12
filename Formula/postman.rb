e2b8d49998f19a8769ec8c9686473f8ba378c07a
okampfer <okampfer@gmail.com>
2018-08-12 23:11:08 +0800

    Upgrade postman to version 6.2.4

--- Formula/postman.rb
+++ Formula/postman.rb
@@ -1,14 +1,14 @@
 class Postman < Formula
     desc "Postman helps you develop APIs faster."
     homepage "https://www.getpostman.com/"
-    version "6.0.10"
+    version "6.2.4"
 
     if MacOS.prefer_64_bit?
         url "https://dl.pstmn.io/download/version/#{version}/linux64"
-        sha256 "82e6debb7bcb436679243581add5cc85cc7c96846c19c76d07a5416e16845a48"
+        sha256 "79d89b92e8e69d0f7080f3fc25cf30108ba2da0b5587b19851d09dd3c2100c3c"
     else
         url "https://dl.pstmn.io/download/version/#{version}/linux32"
-        sha256 "480a699fa2ec8f9d034997ee87e1dccd409fb07388374b253d29cb29f539e523"
+        sha256 "11ad82bf189fdff39c88cfde598870e975c7d04f40096cf78b5c62c7c43e0f02"
     end
 
     bottle :unneeded
