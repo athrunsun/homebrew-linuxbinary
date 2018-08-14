123fec47cd106945bb88af6be3055b1b124e84c2
okampfer <okampfer@gmail.com>
2018-08-14 09:17:23 +0800

    Add back CN mirror for visual-studio-code installation.

--- Formula/visual-studio-code.rb
+++ Formula/visual-studio-code.rb
@@ -3,11 +3,23 @@ class VisualStudioCode < Formula
     homepage "https://code.visualstudio.com/"
     version "1.26"
 
+    option "with-cn-mirror", "Download from Azure China CDN, use this option if you're in mainland China for a faster downloading speed."
+
     if MacOS.prefer_64_bit?
-        url "https://vscode-update.azurewebsites.net/#{version}/linux-x64/stable"
+        if build.with? "cn-mirror"
+            url "https://vscode.cdn.azure.cn/stable/4e9361845dc28659923a300945f84731393e210d/code-stable-1534177726.tar.gz"
+        else
+            url "https://az764295.vo.msecnd.net/stable/4e9361845dc28659923a300945f84731393e210d/code-stable-1534177726.tar.gz"
+        end
+
         sha256 "618ca429e23777ef6b512c0cbd78bc711a7cf6f58752d9f805ced1dc62abb8f0"
     else
-        url "https://vscode-update.azurewebsites.net/#{version}/linux-ia32/stable"
+        if build.with? "cn-mirror"
+            url "https://vscode.cdn.azure.cn/stable/4e9361845dc28659923a300945f84731393e210d/code-stable-1534177733.tar.gz"
+        else
+            url "https://az764295.vo.msecnd.net/stable/4e9361845dc28659923a300945f84731393e210d/code-stable-1534177733.tar.gz"
+        end
+
         sha256 "fd150bee8e1be4f5461013b8fee19f855200d046643e4ea5387aad170ad5f12b"
     end
 
