81f8507a974aa84bb499a0b9c05eebb8757593d3
okampfer <okampfer@gmail.com>
2018-08-14 08:50:28 +0800

    Upgrade visual-studio-code to version 1.26

--- Formula/visual-studio-code.rb
+++ Formula/visual-studio-code.rb
@@ -1,14 +1,14 @@
 class VisualStudioCode < Formula
     desc "VS Code is a new type of tool that combines the simplicity of a code editor with what developers need for their core edit-build-debug cycle. This is the stable version."
     homepage "https://code.visualstudio.com/"
-    version "1.25.1"
+    version "1.26"
 
     if MacOS.prefer_64_bit?
         url "https://vscode-update.azurewebsites.net/#{version}/linux-x64/stable"
-        sha256 "5856bbebf38aa05d584da4722869bbe507cf123f69f7ffab5f1532d73dbf3438"
+        sha256 "618ca429e23777ef6b512c0cbd78bc711a7cf6f58752d9f805ced1dc62abb8f0"
     else
         url "https://vscode-update.azurewebsites.net/#{version}/linux-ia32/stable"
-        sha256 "af6adc2e2500e50bfebe7ee7b97d661b6e774a590136bf5f89334132a5b292e2"
+        sha256 "fd150bee8e1be4f5461013b8fee19f855200d046643e4ea5387aad170ad5f12b"
     end
 
     bottle :unneeded
@@ -16,5 +16,10 @@ class VisualStudioCode < Formula
     def install
         libexec.install Dir["*"]
         bin.install_symlink("#{libexec}/bin/code")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "code".
+        EOS
     end
 end
