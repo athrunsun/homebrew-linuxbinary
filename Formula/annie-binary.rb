0ce7da6ffab6b7d2c03513b010d3ae514cee9788
okampfer <okampfer@gmail.com>
2018-10-16 16:02:39 +0800

    Upgrade annie-binary to version 0.8.2

--- Formula/annie-binary.rb
+++ Formula/annie-binary.rb
@@ -1,14 +1,14 @@
 class AnnieBinary < Formula
     desc "A fast, simple and clean video downloader."
     homepage "https://github.com/iawia002/annie"
-    version "0.7.4"
+    version "0.8.2"
 
     if MacOS.prefer_64_bit?
         url "https://github.com/iawia002/annie/releases/download/#{version}/annie_#{version}_Linux_64-bit.tar.gz"
-        sha256 "12299b7dda1869f8c48e2fdb008d6c18c5b4b0c3e9432df2253e4702361db45d"
+        sha256 "d8d2d3ab9e4c461df9344267f804840cd8570ec10df4afdc6303790b5c6e0df3"
     else
         url "https://github.com/iawia002/annie/releases/download/#{version}/annie_#{version}_Linux_32-bit.tar.gz"
-        sha256 "b38a184a6fefab614d39a0dfc4a98d73cad959047c2a9a12cf1a8b642ebb7d73"
+        sha256 "7e84e5576ed0800a1b75b0b419f348522ca8a19cb7d2415b7efbe5bf6582d418"
     end
 
     bottle :unneeded
