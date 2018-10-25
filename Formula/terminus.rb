3967f5859bf860cf1b0b9e86450ee686b9314954
okampfer <okampfer@gmail.com>
2018-10-25 14:47:03 +0800

    Add a new formula, terminus, version 1.0.0-alpha61

---
+++ Formula/terminus.rb
@@ -0,0 +1,20 @@
+class TerminusBinary < Formula
+    desc "A terminal for a more modern age"
+    homepage "https://github.com/Eugeny/terminus"
+    version "1.0.0.61"
+
+    url "https://github.com/Eugeny/terminus/releases/download/v1.0.0-alpha.61/terminus-1.0.0-alpha.61-linux.tar.gz"
+    sha256 "77af2240afee7f1ed2048a04bb8ddd25b85135662e40660fca4db792886b9974"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/terminus")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "terminus".
+        EOS
+    end
+end
