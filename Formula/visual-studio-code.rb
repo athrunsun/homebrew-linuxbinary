ea188180688cabdec30e6b83ab22823e28a1ba2f
okampfer <okampfer@gmail.com>
2018-11-16 17:22:23 +0800

    Upgrade visual-studio-code to version 1.29.1

--- Formula/visual-studio-code.rb
+++ Formula/visual-studio-code.rb
@@ -1,26 +1,26 @@
 class VisualStudioCode < Formula
     desc "VS Code is a new type of tool that combines the simplicity of a code editor with what developers need for their core edit-build-debug cycle. This is the stable version."
     homepage "https://code.visualstudio.com/"
-    version "1.29.0"
+    version "1.29.1"
 
     option "with-cn-mirror", "Download from Azure China CDN, use this option if you're in mainland China for a faster downloading speed."
 
     if MacOS.prefer_64_bit?
         if build.with? "cn-mirror"
-            url "https://vscode.cdn.azure.cn/stable/5f24c93878bd4bc645a4a17c620e2487b11005f9/code-stable-1542008778.tar.gz"
+            url "https://vscode.cdn.azure.cn/stable/bc24f98b5f70467bc689abf41cc5550ca637088e/code-stable-1542309103.tar.gz"
         else
-            url "https://az764295.vo.msecnd.net/stable/5f24c93878bd4bc645a4a17c620e2487b11005f9/code-stable-1542008778.tar.gz"
+            url "https://az764295.vo.msecnd.net/stable/bc24f98b5f70467bc689abf41cc5550ca637088e/code-stable-1542309103.tar.gz"
         end
 
-        sha256 "8ecbbf632d7e2cd95b0331f7bcbfd304878ce48be6eb95f9d9c8eb060189e2ec"
+        sha256 "31e570bc8ba9e32e2d7ea9a746fb91fbf8a4e011372d99c16c633f4abeacc6e4"
     else
         if build.with? "cn-mirror"
-            url "https://vscode.cdn.azure.cn/stable/5f24c93878bd4bc645a4a17c620e2487b11005f9/code-stable-1542008952.tar.gz"
+            url "https://vscode.cdn.azure.cn/stable/bc24f98b5f70467bc689abf41cc5550ca637088e/code-stable-1542309197.tar.gz"
         else
-            url "https://az764295.vo.msecnd.net/stable/5f24c93878bd4bc645a4a17c620e2487b11005f9/code-stable-1542008952.tar.gz"
+            url "https://az764295.vo.msecnd.net/stable/bc24f98b5f70467bc689abf41cc5550ca637088e/code-stable-1542309197.tar.gz"
         end
 
-        sha256 "aa6972a1670dc5b5b8a0d9ed38d930c4feab783aed0ee13e153cee351ab60a8b"
+        sha256 "9c0fa7448b707be0b55ebc9ebdfa4e90f291d1042ca145ef91e847f3de882964"
     end
 
     bottle :unneeded
