b191b8ab912da9c6e40a11b587a6c247ddecb8c7
okampfer <okampfer@gmail.com>
2018-11-30 22:05:08 +0800

    Add new formula, docker-machine-binary, version 0.16.0

---
+++ Formula/docker-machine-binary.rb
@@ -0,0 +1,23 @@
+class DockerMachineBinary < Formula
+    desc "Machine management for a container-centric world"
+    homepage "https://github.com/docker/machine"
+    version "0.16.0"
+
+    url "https://github.com/docker/machine/releases/download/v#{version}/docker-machine-Linux-x86_64"
+    sha256 "3d2cae5f66df636116153c8a96310c0cdef0060b1ecfe86116cec825bbf393d8"
+
+    bottle :unneeded
+    conflicts_with "docker-machine"
+
+    def install
+        libexec.install Dir["*"]
+        mv("#{libexec}/docker-machine-Linux-x86_64", "#{libexec}/docker-machine")
+        chmod(0755, "#{libexec}/docker-machine")
+        bin.install_symlink("#{libexec}/docker-machine" => "docker-machine")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "docker-machine".
+        EOS
+    end
+end
