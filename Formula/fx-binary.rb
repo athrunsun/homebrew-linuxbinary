80b855c632da856e9bf6a7892bab8d046147cab6
okampfer <okampfer@gmail.com>
2018-12-02 22:12:57 +0800

    Add new formula, fx-binary, version 3.1.0

---
+++ Formula/fx-binary.rb
@@ -0,0 +1,21 @@
+class FxBinary < Formula
+    desc "Command-line tool and terminal JSON viewer"
+    homepage "https://github.com/antonmedv/fx"
+    version "3.1.0"
+
+    url "https://github.com/antonmedv/fx/releases/download/#{version}/fx-linux"
+    sha256 "7c2632ed00aebd70d44e3a7c0bf1e360ca8d7520f4cd37fbd46002a312708137"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        chmod(0755, "#{libexec}/fx-linux")
+        bin.install_symlink("#{libexec}/fx-linux" => "fx")
+    end
+
+    def caveats; <<~EOS
+        Executable linked as "fx".
+        EOS
+    end
+end
