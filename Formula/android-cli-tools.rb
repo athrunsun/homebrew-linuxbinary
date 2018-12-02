3f64f5950eacf3b4db86a8204ac4dc0b4c4f4705
okampfer <okampfer@gmail.com>
2018-12-02 22:12:32 +0800

    Add new formula, android-cli-tools, version 4333796

---
+++ Formula/android-cli-tools.rb
@@ -0,0 +1,20 @@
+class AndroidCliTools < Formula
+    desc "Android command line tools (when you don't need Android Studio)."
+    homepage "https://developer.android.com/studio/"
+    version "4333796"
+    
+    url "https://dl.google.com/android/repository/sdk-tools-linux-#{version}.zip"
+    sha256 "92ffee5a1d98d856634e8b71132e8a95d96c83a63fde1099be3d86df3106def9"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/android" => "android")
+        bin.install_symlink("#{libexec}/emulator" => "emulator")
+        bin.install_symlink("#{libexec}/emulator-check" => "emulator-check")
+        bin.install_symlink("#{libexec}/mksdcard" => "mksdcard")
+        bin.install_symlink("#{libexec}/monitor" => "monitor")
+        bin.install_symlink Dir["#{libexec}/bin/*"]
+    end
+end
