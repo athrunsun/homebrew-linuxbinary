fabf004191b6297a70309ef84d51cb7fa0bfb33a
okampfer <okampfer@gmail.com>
2018-12-04 08:59:59 +0800

    Upgrade firefox to version 63.0.3

--- Formula/firefox.rb
+++ Formula/firefox.rb
@@ -1,15 +1,10 @@
 class Firefox < Formula
-    desc 'The 100% fresh, free-range, ethical browser, with more speed, privacy and freedom. This is the stable version, in en-US.'
+    desc "The 100% fresh, free-range, ethical browser, with more speed, privacy and freedom. This is the stable version, in en-US."
     homepage "https://www.mozilla.org/en-US/firefox/all/"
-    version "61.0.1"
+    version "63.0.3"
 
-    if MacOS.prefer_64_bit?
-        url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2"
-        sha256 "e59fc49056d03c9b614e8131551f9d1979a84459b9896ed42c04a3fd9dad2425"
-    else
-        url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-i686/en-US/firefox-#{version}.tar.bz2"
-        sha256 "3fcd3faf06280dc54bc4544a13d10d40f81d6ef5235624116e846c65000aad13"
-    end
+    url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2"
+    sha256 "0273b63e8bd2901fef31971f0e422e943e04ea86f647e175473bfc668e326bbd"
 
     conflicts_with "firefox-zhcn"
     bottle :unneeded
