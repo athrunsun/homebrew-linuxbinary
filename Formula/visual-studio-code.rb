698e3a99491043239475b730c82f837c4d0a5461
okampfer <okampfer@gmail.com>
2018-12-19 09:41:35 +0800

    Upgrade visual-studio-code to version 1.30.1

--- Formula/visual-studio-code.rb
+++ Formula/visual-studio-code.rb
@@ -1,26 +1,26 @@
 class VisualStudioCode < Formula
     desc "VS Code is a new type of tool that combines the simplicity of a code editor with what developers need for their core edit-build-debug cycle. This is the stable version."
-    homepage "https://code.visualstudio.com/"
-    version "1.30.0"
+    homepage "https://code.visualstudio.com"
+    version "1.30.1"
 
     option "with-cn-mirror", "Download from Azure China CDN, use this option if you're in mainland China for a faster downloading speed."
 
     if MacOS.prefer_64_bit?
         if build.with? "cn-mirror"
-            url "https://vscode.cdn.azure.cn/stable/c6e592b2b5770e40a98cb9c2715a8ef89aec3d74/code-stable-1544567094.tar.gz"
+            url "https://vscode.cdn.azure.cn/stable/dea8705087adb1b5e5ae1d9123278e178656186a/code-stable-1545156735.tar.gz"
         else
-            url "https://az764295.vo.msecnd.net/stable/c6e592b2b5770e40a98cb9c2715a8ef89aec3d74/code-stable-1544567094.tar.gz"
+            url "https://az764295.vo.msecnd.net/stable/dea8705087adb1b5e5ae1d9123278e178656186a/code-stable-1545156735.tar.gz"
         end
 
-        sha256 "818ab50bd1e1d0c93ca2cbb21d3bd8697563fd7399049de79ebd040c9cf376fd"
+        sha256 "8ac8f4149c555def050bedcf32d0a260af6597925d6e8dd69cabb0f40d9be740"
     else
         if build.with? "cn-mirror"
-            url "https://vscode.cdn.azure.cn/stable/c6e592b2b5770e40a98cb9c2715a8ef89aec3d74/code-stable-1544567214.tar.gz"
+            url "https://vscode.cdn.azure.cn/stable/dea8705087adb1b5e5ae1d9123278e178656186a/code-stable-1545156555.tar.gz"
         else
-            url "https://az764295.vo.msecnd.net/stable/c6e592b2b5770e40a98cb9c2715a8ef89aec3d74/code-stable-1544567214.tar.gz"
+            url "https://az764295.vo.msecnd.net/stable/dea8705087adb1b5e5ae1d9123278e178656186a/code-stable-1545156555.tar.gz"
         end
 
-        sha256 "414a02532374e219b990255a14eee06ec4467ef58b148f7586cbee636c93a22d"
+        sha256 "2b01605044ec3b7561505cb782ca214f55fed96e133851e91a856aeef09b4df5"
     end
 
     bottle :unneeded
