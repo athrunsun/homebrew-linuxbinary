b1d2752988fc5387ab7e6df57e7aa5d4a71d5e40
okampfer <okampfer@gmail.com>
2018-12-22 23:20:14 +0800

    Add new formula, sap-machine-jdk, version 11.0.1.13

---
+++ Formula/sap-machine-jdk.rb
@@ -0,0 +1,15 @@
+class SapMachineJdk < Formula
+    desc "An OpenJDK release maintained and supported by SAP"
+    homepage "https://github.com/SAP/SapMachine"
+    version "11.0.1.13"
+
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.1%2B13-0/sapmachine-jdk-11.0.1.13_linux-x64_bin.tar.gz"
+    sha256 "41d70861c71d33ed437d28f3aaa0a22543b83271c35b7910cd8613961dee3d2f"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink Dir["#{libexec}/bin/*"]
+    end
+end
