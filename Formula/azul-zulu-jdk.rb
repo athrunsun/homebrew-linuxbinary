f4340003e72999f3f86fd86bbafa5eb3c44d326d
okampfer <okampfer@gmail.com>
2018-12-22 23:19:44 +0800

    Add new formula, azul-zulu-jdk, version 11.2.3

---
+++ Formula/azul-zulu-jdk.rb
@@ -0,0 +1,15 @@
+class AzulZuluJdk < Formula
+    desc "Zulu Builds of OpenJDK"
+    homepage "https://www.azul.com/downloads/zulu"
+    version "11.2.3"
+
+    url "https://cdn.azul.com/zulu/bin/zulu11.2.3-jdk11.0.1-linux_x64.tar.gz"
+    sha256 "232b1c3511f0d26e92582b7c3cc363be7ac633e371854ca2f2e9f2b50eb72a75"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink Dir["#{libexec}/bin/*"]
+    end
+end
