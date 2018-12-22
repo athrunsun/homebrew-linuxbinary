014db4513682aade80514f88436fd6b7afbf98f9
okampfer <okampfer@gmail.com>
2018-12-22 23:20:37 +0800

    Add new formula, oracle-jdk, version 11.0.1.13

---
+++ Formula/oracle-jdk.rb
@@ -0,0 +1,19 @@
+class OracleJdk < Formula
+    desc "Oracle Java Standard Edition Development Kit."
+    homepage "https://www.oracle.com/technetwork/java/javase/overview/index.html"
+    version "11.0.1.13"
+
+    url "https://download.oracle.com/otn-pub/java/jdk/11.0.1+13/90cf5d8f270a4347a95050320eef3fb7/jdk-11.0.1_linux-x64_bin.tar.gz",
+        cookies: {
+            'oraclelicense' => 'accept-securebackup-cookie',
+        }
+
+    sha256 "e7fd856bacad04b6dbf3606094b6a81fa9930d6dbb044bbd787be7ea93abc885"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink Dir["#{libexec}/bin/*"]
+    end
+end
