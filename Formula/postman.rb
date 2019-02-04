a5f9e822262bf808b165adf8d0310ed31eb02b9f
okampfer <okampfer@gmail.com>
2019-02-04 15:34:41 +0800

    Upgrade postman to version 6.7.3

--- Formula/postman.rb
+++ Formula/postman.rb
@@ -1,10 +1,10 @@
 class Postman < Formula
     desc "Postman helps you develop APIs faster."
-    homepage "https://www.getpostman.com/"
-    version "6.3.0"
+    homepage "https://www.getpostman.com"
+    version "6.7.3"
 
     url "https://dl.pstmn.io/download/version/#{version}/linux64"
-    sha256 "ca197df4deb65b45586039c22f95d6292d643cac48307a4c41879c937c08a526"
+    sha256 "cf413136c5de4a3864fc110404dad5ae65306f618ae01ed3f6c88b79c56aee11"
 
     bottle :unneeded
 
