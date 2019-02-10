42fe427f5414a1a988399c4659a24129f2b03849
okampfer <okampfer@gmail.com>
2019-02-10 17:13:10 +0800

    Upgrade nwjs and nwjs-sdk to version 0.36.1

--- Formula/nwjs-sdk.rb
+++ Formula/nwjs-sdk.rb
@@ -1,10 +1,17 @@
 class NwjsSdk < Formula
     desc "NW.js (previously known as node-webkit) lets you call all Node.js modules directly from DOM and enables a new way of writing applications with all Web technologies. This is the SDK version."
-    homepage "https://nwjs.io/"
-    version "0.26.0"
+    homepage "https://github.com/nwjs/nw.js"
+    version "0.36.1"
 
-    url "https://dl.nwjs.io/v#{version}/nwjs-sdk-v#{version}-linux-x64.tar.gz"
-    sha256 "529d46d200dd7cf321e9c3fb3e1a30d250e574db90c2f10536b37668856f7537"
+    option "with-taobao-mirror", "Download tarball from https://npm.taobao.org/mirrors/nwjs/, use this option if you're in mainland China for a faster downloading speed."
+
+    if build.with? "taobao-mirror"
+        url "https://npm.taobao.org/mirrors/nwjs/v#{version}/nwjs-sdk-v#{version}-linux-x64.tar.gz"
+        sha256 "ab9da4aff7863790c553253b6f15722f00561276c86a2ddae8cd441676a2a390"
+    else
+        url "https://dl.nwjs.io/v#{version}/nwjs-sdk-v#{version}-linux-x64.tar.gz"
+        sha256 "ab9da4aff7863790c553253b6f15722f00561276c86a2ddae8cd441676a2a390"
+    end
 
     bottle :unneeded
 
@@ -15,7 +22,8 @@ class NwjsSdk < Formula
     end
 
     def caveats; <<~EOS
-        Chromium 62 + Node 8.7.0
+        This version contains Chromium 72 + Node 11.9.0.
+        Executable linked as "nw", "nwjc".
         EOS
     end
 end
--- Formula/nwjs.rb
+++ Formula/nwjs.rb
@@ -1,10 +1,17 @@
 class Nwjs < Formula
     desc "NW.js (previously known as node-webkit) lets you call all Node.js modules directly from DOM and enables a new way of writing applications with all Web technologies. This is the normal version."
-    homepage "https://nwjs.io/"
-    version "0.26.0"
+    homepage "https://github.com/nwjs/nw.js"
+    version "0.36.1"
 
-    url "https://dl.nwjs.io/v#{version}/nwjs-v#{version}-linux-x64.tar.gz"
-    sha256 "b812f7ec58a983079d86cc6ce36800fb3891b20939be57e3b948c3f33608b733"
+    option "with-taobao-mirror", "Download tarball from https://npm.taobao.org/mirrors/nwjs/, use this option if you're in mainland China for a faster downloading speed."
+
+    if build.with? "taobao-mirror"
+        url "https://npm.taobao.org/mirrors/nwjs/v#{version}/nwjs-v#{version}-linux-x64.tar.gz"
+        sha256 "031c0a5418e81dd1e16f861327fbfd2227a6411d4b5508d577963081d17dd889"
+    else
+        url "https://dl.nwjs.io/v#{version}/nwjs-v#{version}-linux-x64.tar.gz"
+        sha256 "031c0a5418e81dd1e16f861327fbfd2227a6411d4b5508d577963081d17dd889"
+    end
 
     bottle :unneeded
 
@@ -14,7 +21,8 @@ class Nwjs < Formula
     end
 
     def caveats; <<~EOS
-        Chromium 62 + Node 8.7.0
+        This version contains Chromium 72 + Node 11.9.0.
+        Executable linked as "nw".
         EOS
     end
 end
