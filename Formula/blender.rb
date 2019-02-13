80117989a2596821a62a51acc66779559a09301c
okampfer <okampfer@gmail.com>
2019-02-13 10:02:43 +0800

    Add new formula, blender, version 2.79

---
+++ Formula/blender.rb
@@ -0,0 +1,20 @@
+class Blender < Formula
+    desc "free and open source 3D creation suite."
+    homepage "https://www.blender.org"
+    version "2.79"
+
+    url "https://download.blender.org/release/Blender2.79/blender-2.79b-linux-glibc219-x86_64.tar.bz2"
+    sha256 "43824a4e0b0c6de6fa34ff224eec44c1cc9f26a95f6f3c8c2558d1c05704183c"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/blender")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "blender".
+        EOS
+    end
+end
