f5c479f48d2121c18fba3f93edcc5d1d1ba720e8
okampfer <okampfer@gmail.com>
2019-03-13 23:17:12 +0800

    Upgrade sublime-text to version 3200, we have IME support for fcitx on Ubuntu now!

--- Formula/sublime-text.rb
+++ Formula/sublime-text.rb
@@ -1,15 +1,20 @@
 class SublimeText < Formula
-    desc "Sublime Text is a sophisticated text editor for code, markup and prose. This formula will install from tarball for generic linux platform."
-    homepage "https://www.sublimetext.com/"
-    version "3176"
+    desc "Sublime Text is a sophisticated text editor for code, markup and prose."
+    homepage "https://www.sublimetext.com"
+    version "3200"
 
     url "https://download.sublimetext.com/sublime_text_3_build_#{version}_x64.tar.bz2"
-    sha256 "74f17c1aec4ddec9d4d4c39f5aec0414a4755d407a05efa571e8892e0b9cf732"
+    sha256 "b9d5297c65bd14a8ba3394e74dbd9a5013b3fed5a5a2f8f5120d2cec40614fbd"
 
     bottle :unneeded
 
     def install
         libexec.install Dir["*"]
         bin.install_symlink("#{libexec}/sublime_text" => "subl")
+    end
+
+    def caveats; <<~EOS
+        Executable linked as "subl".
+        EOS
     end
 end
