0bcb4b938f621bafc6bc1890a8971642909ea01a
okampfer <okampfer@gmail.com>
2019-03-13 23:17:33 +0800

    Add new formula, sublime-merge, version 1107.

---
+++ Formula/sublime-merge.rb
@@ -0,0 +1,20 @@
+class SublimeMerge < Formula
+    desc "Git Client, done Sublime."
+    homepage "https://www.sublimemerge.com"
+    version "1107"
+
+    url "https://download.sublimetext.com/sublime_merge_build_#{version}_x64.tar.xz"
+    sha256 "70edbb16529d638ea41a694dbc5b1408c76fcc3a7d663ef0e48b4e89e1f19c71"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/sublime_merge" => "subm")
+    end
+
+    def caveats; <<~EOS
+        Executable linked as "subm".
+        EOS
+    end
+end
