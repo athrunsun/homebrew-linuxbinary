22d248890abb80673a28b875de69913fc7d0db19
okampfer <okampfer@gmail.com>
2019-04-24 18:29:03 +0800

    Add new formula, vscodium, version 1.33.1

---
+++ Formula/vscodium.rb
@@ -0,0 +1,20 @@
+class Vscodium < Formula
+    desc "Binary releases of VS Code without MS branding/telemetry/licensing."
+    homepage "https://github.com/VSCodium/vscodium"
+    version "1.33.1"
+
+    url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium-linux-x64-#{version}.tar.gz"
+    sha256 "ff90d3541627e380afc7026c0ec9b451510a9440e457c951c8a3e3261aefb017"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/bin/vscodium")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "vscodium".
+        EOS
+    end
+end
