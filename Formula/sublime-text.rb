3defc67f0e51b33fc78716b73ec63f97f932fc46
okampfer <okampfer@gmail.com>
2019-04-28 22:54:37 +0800

    Upgrade sublime text to version 3207

--- Formula/sublime-text.rb
+++ Formula/sublime-text.rb
@@ -1,10 +1,10 @@
 class SublimeText < Formula
     desc "Sublime Text is a sophisticated text editor for code, markup and prose."
     homepage "https://www.sublimetext.com"
-    version "3200"
+    version "3207"
 
     url "https://download.sublimetext.com/sublime_text_3_build_#{version}_x64.tar.bz2"
-    sha256 "b9d5297c65bd14a8ba3394e74dbd9a5013b3fed5a5a2f8f5120d2cec40614fbd"
+    sha256 "acb64f1de024a0f004888096afa101051e48d96c7a3e7fe96e11312d524938c4"
 
     bottle :unneeded
 
