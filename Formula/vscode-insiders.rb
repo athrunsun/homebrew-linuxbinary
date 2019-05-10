6658faed8a097068e661e7f22b85e9d0e001ab02
okampfer <okampfer@gmail.com>
2019-05-10 23:16:45 +0800

    Update vscode formulaes to use CDN download links.

--- Formula/vscode-insiders.rb
+++ Formula/vscode-insiders.rb
@@ -1,15 +1,20 @@
 class VscodeInsiders < Formula
     desc "VS Code is a new type of tool that combines the simplicity of a code editor with what developers need for their core edit-build-debug cycle. This is the insiders version."
-    homepage "https://code.visualstudio.com"
-    version "1.18.0"
+    homepage "https://code.visualstudio.com/insiders"
+    version "1.34.20"
 
-    url "https://az764295.vo.msecnd.net/insider/8bc02c7443305a0db442c11b76e67d6f4929dc0e/code-insider-#{version}-1508995529_amd64.tar.gz"
-    sha256 "670042ca762acdfd66bd5f066019fa154893fde45e30569201bc5e9f9ecdef0a"
+    url "https://vscode-update.azurewebsites.net/#{version}/linux-x64/insider"
+    sha256 "a3f2716151d0ef38809afad7f06700885d397c619a0789509ab58664d2d4cca8"
 
     bottle :unneeded
 
     def install
         libexec.install Dir["*"]
-        bin.install_symlink("#{libexec}/bin/code")
+        bin.install_symlink("#{libexec}/bin/code-insiders")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "code-insiders".
+        EOS
     end
 end
--- Formula/vscode.rb
+++ Formula/vscode.rb
@@ -3,14 +3,7 @@ class Vscode < Formula
     homepage "https://code.visualstudio.com"
     version "1.33.1"
 
-    option "with-cn-mirror", "Download from Azure China CDN, use this option if you're in mainland China for a faster downloading speed."
-
-    if build.with? "cn-mirror"
-        url "https://vscode.cdn.azure.cn/stable/51b0b28134d51361cf996d2f0a1c698247aeabd8/code-stable-1554971043.tar.gz"
-    else
-        url "https://az764295.vo.msecnd.net/stable/51b0b28134d51361cf996d2f0a1c698247aeabd8/code-stable-1554971043.tar.gz"
-    end
-
+    url "https://vscode-update.azurewebsites.net/#{version}/linux-x64/stable"
     sha256 "fbf31a814755c54ff744a5da77b5652aa6963a1c3a365a6399cc65ef30935152"
 
     bottle :unneeded
