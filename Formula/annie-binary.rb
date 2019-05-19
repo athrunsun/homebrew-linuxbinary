2234f07ca328d51ee3416c770d4fc320a73dce07
okampfer <okampfer@gmail.com>
2019-05-19 18:37:37 +0800

    Upgrade annie-binary to version 0.9.3

--- Formula/annie-binary.rb
+++ Formula/annie-binary.rb
@@ -1,10 +1,10 @@
 class AnnieBinary < Formula
     desc "A fast, simple and clean video downloader."
     homepage "https://github.com/iawia002/annie"
-    version "0.9.1"
+    version "0.9.3"
 
     url "https://github.com/iawia002/annie/releases/download/#{version}/annie_#{version}_Linux_64-bit.tar.gz"
-    sha256 "79e928dd226780637908763d3cf41f5fe4bad386210ea4e830c83539e26536b3"
+    sha256 "e1173ad44456403e4ce9a95fa6676216870a29b0f3bc45111eb6debf5b96d64f"
 
     bottle :unneeded
     conflicts_with "annie"
