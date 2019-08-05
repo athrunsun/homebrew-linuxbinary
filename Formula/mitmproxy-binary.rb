769617950f7934b7735f10d910668b3b44e26121
okampfer <okampfer@gmail.com>
2019-08-06 00:44:49 +0800

    Add new formula, mitmproxy-binary, version 4.0.4

---
+++ Formula/mitmproxy-binary.rb
@@ -0,0 +1,22 @@
+class MitmproxyBinary < Formula
+    desc "An interactive TLS-capable intercepting HTTP proxy for penetration testers and software developers."
+    homepage "https://github.com/mitmproxy/mitmproxy"
+    version "4.0.4"
+
+    url "https://snapshots.mitmproxy.org/#{version}/mitmproxy-#{version}-linux.tar.gz"
+    sha256 "382c675a439afa97338f5971946cd6fc7b1648350dc9af16aac39884efaa6494"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/mitmproxy")
+        bin.install_symlink("#{libexec}/mitmweb")
+        bin.install_symlink("#{libexec}/mitmdump")
+    end
+
+    def caveats; <<~EOS
+        Executables are linked as "mitmproxy", "mitmweb", "mitmdump".
+        EOS
+    end
+end
