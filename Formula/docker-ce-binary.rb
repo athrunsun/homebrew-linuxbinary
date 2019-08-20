c9e72e67965b16d2bc6abd12f42497ef67f5963c
okampfer <okampfer@gmail.com>
2019-08-20 22:30:38 +0800

    Upgrade docker-ce-binary to version 19.03.1

--- Formula/docker-ce-binary.rb
+++ Formula/docker-ce-binary.rb
@@ -1,10 +1,10 @@
 class DockerCeBinary < Formula
     desc "Pack, ship and run any application as a lightweight container. This is the community edition."
     homepage "https://docs.docker.com/install/linux/docker-ce/binaries"
-    version "18.09.0"
+    version "19.03.1"
 
     url "https://download.docker.com/linux/static/stable/x86_64/docker-#{version}.tgz"
-    sha256 "08795696e852328d66753963249f4396af2295a7fe2847b839f7102e25e47cb9"
+    sha256 "6e7d8e24ee46b13d7547d751696d01607d19c8224c1b2c867acc8c779e77734b"
 
     bottle :unneeded
     conflicts_with "docker", :because => "both install `docker` binaries"
