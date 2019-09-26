b7af71301ca51336d6d56b7082c999b5a97c034c
0x639d1dec74 <JuPlutonic@users.noreply.github.com>
2019-09-26 13:00:05 +0300

    update_ugc_to_75.0.3770.100-1.2
    After update works well. But says what there is newer version of UGC.

--- Formula/ungoogled-chromium.rb
+++ Formula/ungoogled-chromium.rb
@@ -1,10 +1,10 @@
 class UngoogledChromium < Formula
     desc "Google Chromium, sans integration with Google"
     homepage "https://github.com/Eloston/ungoogled-chromium"
-    version "70.0.3538.110"
+    version "75.0.3770.10"
 
-    url "https://dl.opendesktop.org/api/files/download/id/1542708876/s/53b09686e9e33f0677c8c1d702ec9a96b01a1a59ea8b31fdc5ea5cb4a919edfa2cc8aaa6e900fa27413e613cb03a3bd14f4b02eeed8948ec03ba6d562c13da23/t/1549787470/u//ungoogled-chromium_70.0.3538.110-1_linux.tar.xz"
-    sha256 "ba9be1cfe5dd52521c00771034aa0bfbf28716d0b175ba31062fa2b10944472a"
+    url "https://github.com/Eloston/ungoogled-chromium-binaries/releases/download/75.0.3770.100-1.2/ungoogled-chromium_75.0.3770.100-1.2_linux.tar.xz"
+    sha256 "5ac39f58152844ad2f8b19769a52f83c84544b257a93abc2fdfc3a4bf613f9b8"
 
     bottle :unneeded
 
