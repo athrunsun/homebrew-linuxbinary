b58d14393a645748a80085b53903e39292dff0f1
0x639d1dec74 <JuPlutonic@users.noreply.github.com>
2019-09-26 13:16:12 +0300

    update_calibre_to_3.48.0
    After install all works well.

--- Formula/calibre.rb
+++ Formula/calibre.rb
@@ -1,10 +1,10 @@
 class Calibre < Formula
   desc "A powerful and easy to use e-book manager"
   homepage "https://calibre-ebook.com/"
-  version "3.16.0"
+  version "3.48.0"
 
   url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}-x86_64.txz"
-  sha256 "f23f14b8ad4235dd4959a620906b244a4c154ddc35701b90c0a3b111424e02a9"
+  sha256 "79287534d71bee709bc59c93bfb2856f44f8283ceb0e8c9c06b38de8667cfdef"
 
   bottle :unneeded
 
