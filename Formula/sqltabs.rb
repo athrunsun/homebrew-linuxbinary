ed8164612f4c71c63ce38316a5a311b6374fb8b0
0x639d1dec74 <JuPlutonic@users.noreply.github.com>
2019-09-26 12:52:51 +0300

    sqltabs_update_version_to_1.0.0
    After update works well.

--- Formula/sqltabs.rb
+++ Formula/sqltabs.rb
@@ -1,10 +1,10 @@
 class Sqltabs < Formula
     desc "SQL Tabs is an open source cross platform desktop client for postgresql."
     homepage "http://www.sqltabs.com/"
-    version "0.12"
+    version "1.0.0"
 
-    url "http://www.sqltabs.com/download/sqltabs.tar.gz"
-    sha256 "832494d6e6635e0b4c4cde97e1436324a36bbc9deaf37eaa44655388c26d8c42"
+    url "https://github.com/sasha-alias/sqltabs/releases/download/v1.0.0/sqltabs.linux.tar.gz"
+    sha256 "67e15b5d8ed5e2baa176ac650b394c1f0fdeb6dc6e0e493f1ef3acc1569c1c82"
 
     bottle :unneeded
 
