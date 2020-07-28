da66022ddb934f69322e833442629bc0802ee915
ivaquero <46377665+integzz@users.noreply.github.com>
2020-07-28 22:55:53 +0800

    julia-binary: Update to version 1.4.2
    fixed the hash and added the mirror option

--- Formula/julia-binary.rb
+++ Formula/julia-binary.rb
@@ -1,10 +1,17 @@
 class JuliaBinary < Formula
   desc "The Julia programming language"
   homepage "https://julialang.org"
-  version "1.5"
+  version "1.4.2"
 
-  url "https://mirrors.bfsu.edu.cn/julia-releases/bin/linux/x64/1.5/julia-1.5-latest-linux-x86_64.tar.gz"
-  sha256 "A4EA36AA86269116992393067E5AFC182707CB4F26EAC9FDDDA08E04A9C7B94D"
+  option "with-bfsu-mirror", "Download from https://mirrors.bfsu.edu.cn, use this option if you're in mainland China for a faster downloading speed."
+
+    if build.with? "bfsu-mirror"
+        url "https://mirrors.bfsu.edu.cn/julia-releases/bin/linux/x64/1.4/julia-1.4.2-linux-x86_64.tar.gz"
+    else
+        url "https://julialang-s3.julialang.org/bin/linux/x64/1.4/julia-1.4.2-linux-x86_64.tar.gz"
+    end
+
+    sha256 "d77311be23260710e89700d0b1113eecf421d6cf31a9cebad3f6bdd606165c28"
 
   bottle :unneeded
 
