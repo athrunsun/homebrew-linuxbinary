3ab1acbc1b65f087e4be7532bde41ff937773590
okampfer <okampfer@gmail.com>
2021-07-28 16:19:56 +0800

    Add new formula, neovim-binary, version 0.5.0

---
+++ Formula/neovim-binary.rb
@@ -0,0 +1,21 @@
+class NeovimBinary < Formula
+    desc "Vim-fork focused on extensibility and usability"
+    homepage "https://neovim.io"
+    version "0.5.0"
+
+    url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim-linux64.tar.gz"
+    sha256 "cd59e885143e0fa7e43cfdacbc8e37dc033c56d9205b8f8a8acd08cd9f93737d"
+
+    bottle :unneeded
+    conflicts_with "neovim"
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/nvim" => "nvim")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "nvim".
+        EOS
+    end
+end
