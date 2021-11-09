68b4f8865234d2ee8d3f2c878f327ccf43ec8a01
okampfer <okampfer@gmail.com>
2021-11-09 10:31:45 +0800

    Fix wrong url of annie-binary

--- Formula/annie-binary.rb
+++ Formula/annie-binary.rb
@@ -3,10 +3,9 @@ class AnnieBinary < Formula
     homepage "https://github.com/iawia002/annie"
     version "0.11.0"
 
-    url "https://github.com/iawia002/annie/releases/download/#{version}/annie_#{version}_Linux_64-bit.tar.gz"
+    url "https://github.com/iawia002/annie/releases/download/v#{version}/annie_#{version}_Linux_64-bit.tar.gz"
     sha256 "ef247b2e5d318c55657446260e2663908ec8d77677c22ca71aec74384c65c880"
 
-    bottle :unneeded
     conflicts_with "annie"
 
     def install
