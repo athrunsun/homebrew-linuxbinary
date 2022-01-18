684d4d00b9f8d841879d42d8d6919ad4852c1a6f
0x639d1dec74 <JuPlutonic@users.noreply.github.com>
2022-01-18 03:36:03 +0300

    Upd. Discord to 0.0.16

--- Formula/discord.rb
+++ Formula/discord.rb
@@ -1,10 +1,10 @@
 class Discord < Formula
   desc "All-in-one voice and text chat for gamers that's free & secure"
   homepage "https://discordapp.com/"
-  version "0.0.9"
+  version "0.0.16"
 
   url "https://dl.discordapp.net/apps/linux/#{version}/discord-#{version}.tar.gz"
-  sha256 "4e5b955768cb35c0d1564e3bfb174470efc97028a1c38046770266105a440ec4"
+  sha256 "51354a8ecfe2ec2fe6f35e356d706c6a441115dfdcfff126aaa84a864af538e9"
 
   bottle :unneeded
 
