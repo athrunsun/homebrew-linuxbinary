a00c49ed2ca431ab93579f58e5f6831bea5d0d47
okampfer <okampfer@gmail.com>
2022-01-19 22:36:59 +0800

    Upgrade v2ray-core to 4.44.0

--- Formula/v2ray-core.rb
+++ Formula/v2ray-core.rb
@@ -1,10 +1,10 @@
 class V2rayCore < Formula
     desc "A platform for building proxies to bypass network restrictions."
-    homepage "https://github.com/v2ray/v2ray-core"
-    version "4.21.3"
+    homepage "https://github.com/v2fly/v2ray-core"
+    version "4.44.0"
 
-    url "https://github.com/v2ray/v2ray-core/releases/download/v#{version}/v2ray-linux-64.zip"
-    sha256 "7db90a290cd945a2c802fd07907dd9b008b9b26b5d057730e4f1d438965b219a"
+    url "https://github.com/v2fly/v2ray-core/releases/download/v#{version}/v2ray-linux-64.zip"
+    sha256 "c6eb1be207fc5d23c9a18ddb0be9adda6d7255251362863d5ba2177a11528e68"
 
     bottle :unneeded
 
