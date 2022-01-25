e49827227c8a6fae3ff2f15d0086c24d7db02e50
okampfer <okampfer@foxmail.com>
2022-01-26 02:45:39 +0800

    Rename powershell to powershell-binary, upgrade to version 7.2.1, add cn-mirror option.

---
+++ Formula/powershell-binary.rb
@@ -0,0 +1,25 @@
+class PowershellBinary < Formula
+    desc "PowerShell for every system"
+    homepage "https://github.com/PowerShell/PowerShell"
+    version "7.2.1"
+
+    option "with-cn-mirror", "Download from China mainland mirror."
+
+    if build.with? "cn-mirror"
+        url "https://s3.jcloud.sjtu.edu.cn/899a892efef34b1b944a19981040f55b-oss01/github-release/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-linux-x64.tar.gz"
+    else
+        url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-linux-x64.tar.gz"
+    end
+
+    sha256 "337d9864799ad09b46d261071b9f835f69f078814409bc2681f4cc2857b6bda5"
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/pwsh" => "pwsh")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "pwsh".
+        EOS
+    end
+end
