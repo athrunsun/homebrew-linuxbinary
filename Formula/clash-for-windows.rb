f71e6e62106d43ab418bd1a38ef6aa937a61b20f
okampfer <okampfer@gmail.com>
2022-04-05 21:54:30 +0800

    Upgrade telegram-desktop to version 3.6.1

--- Formula/clash-for-windows.rb
+++ Formula/clash-for-windows.rb
@@ -1,10 +1,10 @@
 class ClashForWindows < Formula
     desc "A Windows/macOS/Linux GUI based on Clash and Electron"
     homepage "https://github.com/Fndroid/clash_for_windows_pkg"
-    version "0.19.6"
+    version "0.19.14"
 
     url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}-x64-linux.tar.gz"
-    sha256 "fbd628037a53fe7bafe78eeea4bf36c19320b9e82b8ec5d7c8910ee8ae85133a"
+    sha256 "b0be6a36830ab18ee1545c5e997626736725abf7a1fe46b5c01343c028c6e512"
 
     def install
         libexec.install Dir["*"]
