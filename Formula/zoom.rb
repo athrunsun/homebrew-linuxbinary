cb3b037efb993de7c26504871c73a0d036451966
AMZ <JuPlutonic@users.noreply.github.com>
2022-06-27 13:37:06 +0300

    Update Zoom to 5.11.0 (3540)
    version updated
    sha256 renewed
    libcef.so path appended

--- Formula/zoom.rb
+++ Formula/zoom.rb
@@ -1,10 +1,10 @@
 class Zoom < Formula
     desc "Video Conferencing, Web Conferencing, Webinars, Screen Sharing"
     homepage "https://zoom.us"
-    version "5.9.6.2225"
+    version "5.11.0.3540"
 
     url "https://zoom.us/client/#{version}/zoom_x86_64.tar.xz"
-    sha256 "0af655c5935a887f68a33968c3c54dcf4e8e530f95ea18b0ff2f254c49b18dcc"
+    sha256 "b7b812f151ab303a2ca1cff581c29298fde36c1664855221e4f511ae228fe502"
 
     def install
         libexec.install Dir["*"]
@@ -12,7 +12,7 @@ class Zoom < Formula
             #!/usr/bin/env bash
             ZOOM_PATH=$(brew --prefix zoom)/libexec
             cd $ZOOM_PATH
-            LD_LIBRARY_PATH=$ZOOM_PATH ./zoom
+            LD_LIBRARY_PATH=$ZOOM_PATH:$ZOOM_PATH/cef ./zoom
         EOS
         chmod(0755, "#{libexec}/zoom_brew_exec")
         bin.install_symlink("#{libexec}/zoom_brew_exec" => "zoom")
